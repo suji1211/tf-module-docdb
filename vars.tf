bcffbc071da4b9d609218ae79673c46b6314a7ee
suji <borra.sujani@gmail.com>
2023-06-12 09:48:30 -0400

    prometheus

---
+++ vars.tf
@@ -0,0 +1,14 @@
+variable "env" {}
+variable "subnets" {}
+variable "name" {
+  default = "docdb"
+}
+variable "vpc_id" {}
+variable "allow_db_cidr" {}
+variable "engine_version" {}
+variable "kms_arn" {}
+variable "port_no" {
+  default = 27017
+}
+variable "instance_count" {}
+variable "instance_class" {}
